bccb06db421833fdde759f36c957bf21534ba19a
AdventureCo <hello@fuzzspot.com>
2020-08-30 19:17:15 -0400

    Create module

---
+++ src/modules/admin/MetaLog/MetaLog.ts
@@ -0,0 +1,45 @@
+import { ModuleInterface } from 'modules'
+import { DiscordServer } from 'DiscordServer'
+import { logger } from 'utility/logger'
+import PubSub from 'pubsub-js'
+import Discord, { RichEmbed } from 'discord.js'
+
+export class MetaLog implements ModuleInterface {
+  private readonly server: DiscordServer
+  private readonly channel: Discord.TextChannel
+
+  private constructor () {
+    this.server = DiscordServer.getInstance()
+    this.channel = this.server.getChannel('CHANNEL_METALOG')
+  }
+
+  public apply (): void {
+    this.listenAndLog()
+  }
+
+  /**
+   * Listens for events and logs 'em
+   */
+  private listenAndLog (): void {
+    const channel = this.channel
+
+    PubSub.subscribe('logMeta', function (_event: String, data: { module: string, message: string, extra?: string }) {
+      if (!('module' in data) || !('message' in data)) {
+        return
+      }
+
+      const embed = new RichEmbed()
+        .setTitle(data.module)
+        .setColor('GREEN')
+        .setDescription(data.message)
+
+      if ('extra' in data) {
+        embed.addField('Additional', data.extra)
+      }
+
+      channel.send(embed).catch(e => {
+        logger.log('error', e.message, ...[e.data])
+      })
+    })
+  }
+}
---
+++ src/modules/admin/MetaLog/README.md
@@ -0,0 +1,5 @@
+## MetaLog module
+Logs module actions with pub/sub to explain why a particular action was taken ( ex, muting a user )
+
+### Environment variables
+- `CHANNEL_METALOG` metalog channel ID 
