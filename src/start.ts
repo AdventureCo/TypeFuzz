26618d09f06609e62d256e66c76721b96145ccdb
AdventureCo <hello@fuzzspot.com>
2021-02-01 01:51:56 -0500

    Remove jobs folder + code

--- src/start.ts
+++ src/start.ts
@@ -1,6 +1,5 @@
 import { MongoHelper } from 'MongoHelper'
 import { logger } from 'utility/logger'
-import * as cronJobs from 'jobs'
 import { DiscordServer } from 'DiscordServer'
 
 // Connect to MongoDB Database
@@ -10,14 +9,6 @@ MongoHelper.getInstance().connect().then(() => {
 
   // init our server singleton
   DiscordServer.getInstance()
-
-  // init all our cron jobs
-  for (const name of Object.keys(cronJobs)) {
-    const job = (cronJobs as any)[name]
-    if (typeof job === 'object') {
-      job.start()
-    }
-  }
 }).catch(error => {
   logger.log('error', error)
 })
